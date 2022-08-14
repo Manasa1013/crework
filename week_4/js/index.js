8db4bbe840d4bdd2c711858a08af68a0b3cb769d
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2022-08-14 19:55:14 +0530

    add : password validation

--- week_4/js/index.js
+++ week_4/js/index.js
@@ -10,47 +10,63 @@ let passwordInputValue = passwordInput.value;
 const isShownButton = document.querySelector("#is--shown");
 let isShownButtonValue = isShownButton.value;
 
+const agreeCheckBox = document.querySelector("#agree--checkbox");
+let agreeCheckBoxValue = agreeCheckBox.checked;
+
 let showPassword = false;
+let checked = false;
+let nameRegx = /^([a-zA-Z0-9\s-]+)$/;
+let emailRegx = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,5})$/;
+let passwordRegx = /^(?=.*[0-9])(?=.*[A-Z]).{4,12}$/;
 
 try {
   const nameContainer = document.querySelector("#name--container");
   const nameInput = document.querySelector("#name--input");
   let nameInputValue = nameInput.value;
-  let nameAlert = document.createElement("div");
-  nameContainer.appendChild(nameAlert);
+  let nameAlert = document.querySelector("#name--alert");
 
-  nameInput.addEventListener("input", (e) => {
-    nameInputValue = e.target.value;
-    console.log(`nameInputValue,${nameInputValue}`);
-  });
-  nameInput.addEventListener("change", () => {
-    if (nameInputValue.length <= 1 && nameInputValue.length > 30) {
-      console.log(`nameInputValue consists of one letter`);
-      nameInput.style.borderColor = "var(--error-red)";
-      nameAlert.innerHTML = `<span> Must be an appropriate name</span>`;
-    }
-  });
+  nameInput.addEventListener(
+    "input",
+    (e) => {
+      nameInputValue = e.target.value;
+      console.log(`nameInputValue,${nameInputValue}`);
+    },
+    false
+  );
+  nameInput.addEventListener(
+    "change",
+    (eve) => {
+      eve.preventDefault();
+
+      if (!nameRegx.test(nameInputValue)) {
+        showErrorAlert(nameAlert, nameInput);
+        nameAlert.innerHTML = `<span ><i class="fa-solid fa-circle-xmark"></i>  Must be an appropriate name</span>`;
+      } else if (nameInputValue.length === 0) {
+        showErrorAlert(nameAlert, nameInput);
+        nameAlert.innerHTML = `<span ><i class="fa-solid fa-circle-xmark"></i> Name can't be blank</span>`;
+      }
+    },
+    false
+  );
 } catch (e) {
   console.error("It's login form and name input field doesn't exist", e);
 }
 
-let emailAlert = document.createElement("div");
-let passwordAlert = document.createElement("div");
-
-emailContainer.appendChild(emailAlert);
-passwordContainer.appendChild(passwordAlert);
-
-emailAlert.style.color = "var(--error-red)";
+let emailAlert = document.querySelector("#email--alert");
+let passwordAlert = document.querySelector("#password--alert");
 
 emailInput.addEventListener("input", (e) => {
   emailInputValue = e.target.value;
   console.log(`value : ${emailInputValue}`);
 });
-emailInput.addEventListener("change", () => {
-  if (!emailInputValue.includes("@")) {
+emailInput.addEventListener("change", (eve) => {
+  eve.preventDefault();
+  if (!emailRegx.test(emailInputValue)) {
     emailAlert.innerHTML = `<span><i class="fa-solid fa-circle-xmark"></i> Must be an email</span>`;
+    showErrorAlert(emailAlert, emailInput);
   } else if (emailInputValue.length === 0) {
-    emailAlert.innerHTML = `<span><i class="fa-solid fa-triangle-exclamation"></i> Email can't be blank`;
+    showErrorAlert(emailAlert, emailInput);
+    emailAlert.innerHTML = `<span><i class="fa-solid fa-triangle-exclamation"></i> Email can't be blank</span>`;
   }
 });
 
@@ -58,11 +74,17 @@ passwordInput.addEventListener("input", (e) => {
   passwordInputValue = e.target.value;
   console.log(`password : ${passwordInputValue}`);
 });
-passwordInput.addEventListener("change", () => {
-  console.log(`password on chahen${passwordInputValue}`);
+passwordInput.addEventListener("change", (eve) => {
+  eve.preventDefault();
+  console.log(`password on change${passwordInputValue}`);
+  if (passwordInputValue.length === 0) {
+    showErrorAlert(passwordAlert, passwordInput);
+    passwordAlert.innerHTML = `<span><i class="fa-solid fa-triangle-exclamation"></i> Password can't be blank</span>`;
+  } else if (!passwordRegx.test(passwordInputValue)) {
+    showErrorAlert(passwordAlert, passwordInput);
+    passwordAlert.innerHTML = `<span><i class="fa-solid fa-triangle-exclamation"></i> Password is invalid</span>`;
+  }
 });
-
-//toggling eye and eye-slash icons
 
 isShownButton.addEventListener("click", () => {
   if (showPassword === false) {
@@ -74,3 +96,12 @@ isShownButton.addEventListener("click", () => {
   }
   showPassword = !showPassword;
 });
+
+agreeCheckBox.addEventListener("change", () => {
+  console.log(agreeCheckBox.checked, "agreeCheckBox checked value");
+  checked = !checked;
+});
+function showErrorAlert(alertField, entryField) {
+  alertField.classList.add("alert");
+  entryField.style.borderColor = "var(--error-red)";
+}
