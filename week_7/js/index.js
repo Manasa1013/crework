b0a8837d79ba5cc2655bd176f7ba07066f780dc4
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2022-11-04 15:37:20 +0530

    create App styles and components

--- week_7/js/index.js
+++ week_7/js/index.js
@@ -3,6 +3,9 @@ const searchButton = document.querySelector("#search-button");
 
 let searchInputValue = searchInput.value;
 
+const id = "222026c437384544b7fb802e7dbba16c";
+const token = "7b1f659c15b74166854b42766fef0d22";
+
 searchInput.addEventListener("change", (e) => {
   console.log(e.target.value);
   searchInputValue = e.target.value;
