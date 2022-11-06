ff481d7332171bbecc0ba304ad5dc2cf642212f9
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2022-11-06 12:38:45 +0530

    refactor : cross  button functionality

--- react_week1/signandlogin/src/components/Toast/Toast.jsx
+++ react_week1/signandlogin/src/components/Toast/Toast.jsx
@@ -1,5 +1,12 @@
 import "./Toast.css";
-
-export function Toast({ message }) {
-  return <div className="toast show">Toast {message}</div>;
+import "../SignupForm/SignupForm.css";
+export function Toast({ toast, hideToastBar }) {
+  return (
+    <div className={`toast ${toast.isVisible}`}>
+      {toast.message}
+      <button className="icon--button" onClick={() => hideToastBar()}>
+        <em className="fa fa-xmark icon--xmark"></em>
+      </button>
+    </div>
+  );
 }
