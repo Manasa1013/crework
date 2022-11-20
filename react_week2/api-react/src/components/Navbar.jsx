6376aa6787369a8a2f43ea8c5091d11367c10994
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2022-11-20 11:50:45 +0530

    feat :add artists display and other routes

---
+++ react_week2/api-react/src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <>
+      <nav className="navbar-component">
+        <div className="route-links">
+          <div
+            className={
+              openMenu === true ? "hamburger-menu open" : "hamburger-menu"
+            }
+            onClick={() => setOpenMenu((prev) => !prev)}
+          >
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+          </div>
+          <header className="inline">
+            <a style={{ padding: "0 1rem" }} href="/" className="logo link">
+              Spotify
+            </a>
+          </header>
+          <ul
+            style={{ padding: "0 0.5rem" }}
+            className={
+              openMenu ? "categories__list active" : "categories__list"
+            }
+          >
+            <li>
+              <Link to="/artists" className="link">
+                Artists
+              </Link>
+            </li>
+            <li>
+              <Link to="/songs" className="link">
+                Songs
+              </Link>
+            </li>
+            <li>
+              <Link to="/playlists" className="link">
+                Playlists
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <ul className="user__list">
+          <li>
+            <a href="/" className="link">
+              <em className="fa-solid fa-heart"></em>
+            </a>
+          </li>
+          <li>
+            <a href="https://reactsigninandlogin.netlify.app/" className="link">
+              <em className="fa fa-user"></em>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
